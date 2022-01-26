415837b86ae729969a2d839862fea86ba7184d9b
Vitovtas Buzas <vitavtasbuzas@gmail.com>
2022-01-26 03:45:06 +0300

    Done homework at 3:44 26.01.2022

--- src/homework/firstLesson/Box.java
+++
@@ -1,4 +0,0 @@
-package homework.firstLesson;
-
-public class Box {
-}
--- src/homework/firstLesson/fruits/Apple.java
+++ src/homework/firstLesson/fruits/Apple.java
@@ -1,4 +1,18 @@
 package homework.firstLesson.fruits;
 
-public class Apple {
+public class Apple implements Fruit {
+
+    Double weight = 1.0;
+
+    public Apple() {
+        this.weight = weight;
+    }
+
+
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+
+
 }
